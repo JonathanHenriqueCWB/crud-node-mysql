2ca9be76a67aa8881bb9c0753803c27be54dd00d
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2021-05-12 16:45:41 -0300

    Configurando template engine (handlebars) e criando as views

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
 
-app.listen(8080, function(){
+// Configurando o template engine (handlebars)
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', function (req, res) {
+    res.render('home')
+})
+
+app.get('/cadastrar', function(req, res) {
+    res.render('formulario')
+})
+
+app.listen(8080, function () {
     console.log('Server is runnig on port 8080')
 })
+
+/**
+ *  1 - Instalar o express: npm install --save express
+ *  2 - Instalar o banco de dados npm install --save mysql2
+ *  3 - Instalar o ORM (Sequelize) npm install --save sequelize
+ *  4 - Instalar o template engine: npm install --save express-handlebars
+ *
+ *  Opcional instalar o nodemon: npm install -g --save-dev nodemon  *
+ */
